210772a4e8321b7a91c32fe45a634c52f1f16ee9
bilalBEN167 <bilalx2017@gmail.com>
2025-12-23 16:31:38 +0100

    Update ActivateKSP.ps1

--- ActivateKSP.ps1
+++ ActivateKSP.ps1
@@ -1,3 +1,3 @@
 $kesPath = "C:\Program Files (x86)\Kaspersky Lab\KES.12.10.0"
 Set-Location $kesPath
-Start-Process -FilePath "avp.exe" -ArgumentList "ACTIVATE HQCUR-MK19R-PNUB4-NTMU2" -Wait -NoNewWindow
+Start-Process -FilePath "avp.com" -ArgumentList "ACTIVATE HQCUR-MK19R-PNUB4-NTMU2" -Wait -NoNewWindow
