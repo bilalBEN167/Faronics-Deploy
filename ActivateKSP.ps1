9e10a97c93269e01c829fffba57618ac82984ac4
bilalBEN167 <bilalx2017@gmail.com>
2025-12-23 16:33:56 +0100

    Delete ActivateKSP.ps1

--- ActivateKSP.ps1
+++
@@ -1,3 +0,0 @@
-$kesPath = "C:\Program Files (x86)\Kaspersky Lab\KES.12.10.0"
-Set-Location $kesPath
-Start-Process -FilePath "avp.com" -ArgumentList "ACTIVATE HQCUR-MK19R-PNUB4-NTMU2" -Wait -NoNewWindow
