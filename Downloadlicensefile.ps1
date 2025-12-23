b03562a156dfd9a4f3054c538a625747c1faef3f
bilalBEN167 <bilalx2017@gmail.com>
2025-12-23 17:03:56 +0100

    Update Downloadlicensefile.ps1

--- Downloadlicensefile.ps1
+++ Downloadlicensefile.ps1
@@ -1,6 +1,8 @@
-# Download license file
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bilalBEN167/Faronics-Deploy/refs/heads/main/KasperskyEndpointLicen.key" -OutFile "C:\Temp\KasperskyEndpointLicen.key"
+# Créer le dossier Temp si absent
+New-Item -ItemType Directory -Path "C:\Temp" -Force
 
-# Apply license
-Start-Process -FilePath "C:\Program Files (x86)\Kaspersky Lab\KES.12.10.0\avp.com" `
-  -ArgumentList "ADDKEY C:\Temp\KasperskyEndpointLicen.key" -Wait -NoNewWindow
+# Télécharger le fichier .key
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bilalBEN167/yourrepo/main/KasperskyEndpointLicen.key" -OutFile "C:\Temp\KasperskyEndpointLicen.key"
+
+# Activer la licence
+Start-Process -FilePath "C:\Program Files (x86)\Kaspersky Lab\Kaspersky Endpoint Security for Windows\avp.com" -ArgumentList "ADDKEY C:\Temp\KasperskyEndpointLicen.key" -Wait -NoNewWindow
